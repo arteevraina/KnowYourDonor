d99bbee498988db907dff356893c1e73aff18a78
antrikshmisri <antrikshmisri@gmail.com>
2021-01-05 19:05:35 +0530

    feat: added screens and component dir

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+
       title: 'Flutter Demo',
       theme: ThemeData(
         // This is the theme of your application.
