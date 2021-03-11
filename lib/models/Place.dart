51b1c712c006bbd729cd5539c7e3ca117b3723d6
Arteev Raina <arteevraina@gmail.com>
2021-03-11 14:42:49 +0530

    feat: added Prediction screen and changed firebase project

---
+++ lib/models/Place.dart
@@ -0,0 +1,14 @@
+// Model for Place.
+class Place {
+  Place(
+    this.placeId,
+    this.address,
+    this.lat,
+    this.long,
+  );
+
+  final String placeId;
+  final String address;
+  final double lat;
+  final double long;
+}
